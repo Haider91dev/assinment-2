f569710d8fd3c0854392a37f2e440fbe60ba49d4
Haider91dev <154570409+Haider91dev@users.noreply.github.com>
2024-01-03 08:15:37 +0530

    assinment-2

---
+++ app.js
@@ -0,0 +1,44 @@
+// alert("jj")
+
+// assinment 2
+
+//Q1
+ var userName;
+
+ //Q2
+ var myName ="Haider Ali";
+
+ //Q3 
+ //a
+ var message;
+ //b
+ message ="Hello World";
+ //c
+ //  alert(message);
+
+ //Q4
+ var name ="johan cena";
+ var age = "15 year old";
+ var course = "Certified Mobile Application Development";
+ //   alert(name);
+ //   alert(age)
+ //   alert(course)
+
+ // Q5
+
+//  alert("pizza\npizz\npiz\npi\np")
+
+//Q6
+var email  ="xyz@gmail.com"
+// alert("My email address is"+'  ' + email);
+
+
+//Q7
+var book = "A Smart way to learn javascript"
+// alert("I am trying to learn from a Book  "+book);
+
+// Q8
+
+// Q9
+// how to make this string ???
+
